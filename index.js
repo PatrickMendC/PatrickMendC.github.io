2786463eb7751a3c90e5434dda7fcc5fcd39f945
PatrickMendC <milhionniverso@gmail.com>
2019-08-07 15:14:15 -0400

    corrigido o formato da data

---
+++ index.js
@@ -0,0 +1,131 @@
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+const Feed = require('feed').Feed;
+var fs = require('fs');
+//array de dados
+const data = [];
+
+const options = {
+    url: 'http://www.ifms.edu.br/noticias',
+    transform: function (body) {
+        return cheerio.load(body)
+    }
+}
+//criando novo feed 
+const feed = new Feed({
+    title: "Noticias Ifms",
+    description: "Este é um feed de noticias do ifms!",
+    id: "https://ifms.edu.br.com/",
+    link: "https://ifms.edu.br/noticias",
+    language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+    image: "http://example.com/image.png",
+    favicon: "http://example.com/favicon.ico",
+    copyright: "All rights reserved 2019, Ifms",
+    updated: new Date(2019, 7, 18), // optional, default = today
+    generator: "awesome", // optional, default = 'Feed for Node.js'
+    feedLinks: {
+      json: "https://example.com/json",
+      atom: "https://example.com/atom"
+    },
+    author: {
+      name: "John Doe",
+      email: "johndoe@example.com",
+      link: "https://example.com/johndoe"
+    }
+  });
+
+console.log(feed.rss2());
+
+function processarDados(dados){
+  var time = new Date(dados[1])
+    console.log("Dados:"+time)
+    
+    //var teste = [2,4,6]
+    dados.forEach(d => {
+        feed.addItem({
+          title: d.nome,
+          id: d.url,
+          link: d.url,
+          description: d.descricao,
+          content: d.content,
+          author: [
+            {
+              name: "Ifms",
+              email: "ifms@example.com",
+              link: "https://example.com/ifms"
+            }
+          ],
+          contributor: [
+            {
+              name: "Shawn Kemp",
+              email: "shawnkemp@example.com",
+              link: "https://example.com/shawnkemp"
+            }
+          ],
+          date: new Date(d.date),
+          image: d.img+" type='image/jpeg'"
+        });
+      });
+      //console.log(feed.rss2());
+    //console.log(JSON.stringify(dados))
+    
+    
+    //fs.readFile('rss.json', 'utf8', function readFileCallback(err, dados){
+    //    if (err){
+    //        console.log(err);
+    //    } else {
+    //        var json = JSON.stringify(dados);
+    //        fs.writeFile('rss.json', json, 'utf8', callback);
+    //    }
+    //})
+    fs.writeFile("rss.xml", feed.rss2(), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Arquivo xml salvo com sucesso!");
+        //console.log(feed.rss2());
+    })
+
+    fs.writeFile("rss.json", JSON.stringify(dados), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Arquivo json salvo com sucesso!");
+    })
+    
+}
+
+// O formato da data contida no argumento "data" é dd/mm/yyyy como uma String
+// inverteDate inverte a data brasileira para formato americano, e a retorna como uma Date
+const inverteDate = (data) =>{
+  split = data.split('/');
+  novadata = split[1] + "/" +split[0]+"/"+split[2];
+  data_americana = new Date(novadata);
+  
+  return data_americana;
+}
+
+rp(options)
+    .then(($) => {
+        //console.log($);
+        const titles = []
+        $('.tileItem').each((i, item) => {
+            const title = {
+                nome: $(item).find('.tileHeadline').text(),
+                descricao: $(item).find('.description').text(),
+                img: $(item).find('.tileImage').children('img').eq(0).attr('src'),
+                //date: $(item).find('.documentByLine, <i>').children('.summary-view-icon').text()
+                date: $(item).find('.summary-view-icon').first().text().replace(/(\n)/g, ""),
+            }
+            console.log("Formato data: "+inverteDate(title.date))
+            if(title.nome !== "")
+                titles.push(title)
+        })
+
+        //processarDados(JSON.stringify(titles))
+        processarDados(titles)
+        //setDados(titles)
+    })
+    .catch((err) => {
+        console.log(err);
+    })
